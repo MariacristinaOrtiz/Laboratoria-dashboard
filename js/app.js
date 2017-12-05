62a972b60c4cb197c0b14b12e2d5071cb339cee6
MariacristinaOrtiz <mari_d17@hotmail.com>
2017-12-05 15:02:35 -0500

    añade modificaciones js

--- js/app.js
+++ js/app.js
@@ -23,18 +23,18 @@ var centers = [
 function getCohorts() {
   for (var i = 0; i < centers.length; i++) {
     // cohortes por sede
-    keys = Object.keys(data[centers[i].key]);
+    keys = Object.keys(data[centers[i].key]); // devuelve un array de las generaciones y estan alamacenadas en keys
     // numero de cohortes
-    centers[i].cohorts = keys.length;
+    centers[i].cohorts = keys.length; // longitud de las generaciones  para recorrer las generaciones
     // cohortes
-    centers[i].cohortsList = keys;
+    centers[i].cohortsList = keys; // se le asigna la propiedad cohortlist y se le da valor de keys
   }
 }
 
 getCohorts();
 var ZERO = 0;
-var campusDefault = centers[ZERO].key;
-var cohortDefault = centers[ZERO].cohortsList[ZERO];
+var campusDefault = centers[ZERO].key;// sera AQP
+var cohortDefault = centers[ZERO].cohortsList[ZERO]; // primera posicion de AQP
 
 // Función que retorna estudiantes del campus y cohort seleccionado
 function getStudents(data, campus = campusDefault, cohort = cohortDefault) {
